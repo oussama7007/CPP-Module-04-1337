87dc5cbd367cf133ca339174000ddf25956e98c3
oussama7007 <oussamatachakourt@gmail.com>
2025-12-09 15:32:35 +0100

    Auto update: 2025-12-09 15:32:35

--- ex00/Cat.cpp
+++ ex00/Cat.cpp
@@ -6,25 +6,34 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 13:53:59 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/12/09 14:32:26 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/12/09 14:51:52 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
 #include "Cat.h"
 
-Cat::Cat() :type("Cat"), Animal()
-{
-    // message
+Cat::Cat() : Animal() {
+    this->type = "Cat";
+    std::cout << "Cat Constructor" << std::endl;
 }
 
-Cat::Cat(const Cat& obj): Animal(obj)
-{
-    // message
-    *this = obj;
+Cat::Cat(const Cat& other) : Animal(other) {
+    std::cout << "Cat Copy Constructor" << std::endl;
+    *this = other;
 }
 
-Cat& operator=(const Cat& obj) 
-{
-    if(this != obj)
-        Animel::operator(obj);
+Cat& Cat::operator=(const Cat& other) {
+    std::cout << "Cat Assignment Operator" << std::endl;
+    if (this != &other)
+        Animal::operator=(other);
+    return *this;
+}
+
+Cat::~Cat() {
+    std::cout << "Cat Destructor" << std::endl;
+}
+
+void Cat::makeSound() const {
+    std::cout << "Meow!" << std::endl;
 }
