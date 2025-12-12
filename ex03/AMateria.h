7d5f4007ee33ff0202dd084db0dbca9586896b5a
Oussama Ait Si Hamou <oait-si-@e3r3p1.1337.ma>
2025-12-12 19:09:55 +0100

    Auto update: 2025-12-12 19:09:55

---
+++ ex03/AMateria.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   AMateria.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/12 18:05:11 by oait-si-          #+#    #+#             */
+/*   Updated: 2025/12/12 18:12:36 by oait-si-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+
+
+class ICharacter; 
+
+class AMateria
+{
+    protected:
+        std::string type;
+    public:
+        AMateria(std::string const & type);
+        AMateria(const AMateria& other);
+        AMateria& operator=(const AMateria& other);
+        virtual ~AMateria();
+
+        std::string const & getType() const; // Returns the type
+        
+        virtual AMateria* clone() const = 0; // Pure virtual
+        virtual void use(ICharacter& target);
+};
--- ex03/Ice.cpp
+++ ex03/Ice.cpp
@@ -1,26 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ICharacter.h                                       :+:      :+:    :+:   */
+/*   Ice.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/12/12 18:06:56 by oait-si-          #+#    #+#             */
+/*   Created: 2025/12/12 18:11:24 by oait-si-          #+#    #+#             */
 /*   Updated: 2025/12/12 18:12:36 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
-#pragma once 
-
-#include <iostream>
-
-class ICharacter
-{
-public:
-virtual ~ICharacter() {}
-virtual std::string const & getName() const = 0;
-virtual void equip(AMateria* m) = 0;
-virtual void unequip(int idx) = 0;
-virtual void use(int idx, ICharacter& target) = 0;
-};
