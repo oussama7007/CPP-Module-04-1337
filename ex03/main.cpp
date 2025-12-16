3be9538470b3ae640cb7e4e4b7d19786fd761fa6
Oussama Ait Si Hamou <oait-si-@e3r2p2.1337.ma>
2025-12-16 17:06:41 +0100

    finish

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,11 +6,9 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 17:08:48 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/12/16 16:47:34 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/12/16 17:04:16 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-
 
 #include "AMateria.h"
 #include "Character.h"
@@ -19,7 +17,6 @@
 #include "ICharacter.h"
 #include "IMateriaSource.h"
 #include "MateriaSource.h"
-
 
 
 void testSubject() {
@@ -78,70 +75,38 @@ void testMateriaSource() {
     delete overflow;
 }
 
-void testInventory() {
-    std::cout << "\n========================================" << std::endl;
-    std::cout << "    TEST 3: INVENTORY & UNEQUIP" << std::endl;
-    std::cout << "========================================" << std::endl;
 
-    Character* me = new Character("Gamer");
-    AMateria* m1 = new Ice();
-
-    std::cout << "[1] Equipping item..." << std::endl;
-    me->equip(m1);
-    
-    std::cout << "[2] Unequipping item (Logic Check)..." << std::endl;
-    me->unequip(0); 
-    
-    std::cout << "[3] Re-equipping (finding empty slot)..." << std::endl;
-    me->equip(m1); 
-
-  
-    me->equip(new Cure());
-    me->equip(new Ice());
-    me->equip(new Cure()); // Now full (4 items)
-
-    std::cout << "[4] Equipping 5th item (Should be rejected)..." << std::endl;
-    AMateria* extra = new Ice();
-    me->equip(extra); // Should do nothing
-
-    delete extra; 
-    delete me;    
-}
 
 void testDeepCopy() {
     std::cout << "\n========================================" << std::endl;
-    std::cout << "       TEST 4: DEEP COPY CHECK" << std::endl;
+    std::cout << "       TEST 3: DEEP COPY CHECK" << std::endl;
     std::cout << "========================================" << std::endl;
 
     Character* original = new Character("Original");
     original->equip(new Ice());
     original->equip(new Cure());
 
-    // Copy Constructor
+    
     Character* copy = new Character(*original);
 
-    // DELETE ORIGINAL
+    
     delete original;
     std::cout << "Original deleted. Testing copy survival..." << std::endl;
 
-    // Verify copy still works (Accessing its own memory)
-    // If deep copy failed, this crashes here (using deleted pointer).
+
     copy->use(0, *copy); 
     copy->use(1, *copy);
 
     delete copy;
 }
 
-void    f()
-{
-    system("leaks Materia");
-}
+
 
 int main() {
-    atexit(f);
+
     testSubject();
     testMateriaSource();
-    testInventory();
+  
     testDeepCopy();
     
 
